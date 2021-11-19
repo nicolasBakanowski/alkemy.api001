1c4fa3c4e2316bc6be40118977f51bff20ccb30e
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2021-11-19 17:27:02 -0300

    fix fk bug

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,11 +1,22 @@
 const { response, request } = require('express')
+const { Image } = require('image-js');
+
 const Post = require('../models/postModel')
+const Category = require ('../models/categoryModel')
 require('dotenv') 
 
 
 
-async function createPost(request,response){
-    console.log(request.body)
+async function createPost(request,response){     
+    const existCategory = await Category.count({
+        where: {
+            id: request.body.categories_id
+        }
+    })
+
+    if (existCategory < 1) return response.status(400).send({status: "this category don't exists"})
+    
+
     const existPost =  await Post.count({
         where: {
             title: request.body.title
@@ -19,13 +30,31 @@ async function createPost(request,response){
 }
 
 async function getPosts(request,response){
-    //const post = await Post.findAll({
-    //    order:[
-    //        ['creation_date', 'DESC'],
-    //    ]
-    //});
+    Post.belongsTo(Category, {foreignKey: 'categories_id'})
+    const post = await Post.findAll({
+        include:[{
+            model: Category,
+        }],
+        order:[
+            ['createdAt', 'DESC'],
+        ]
+    });
     if (post.length <1) return response.status(404).send({status:'No hay post que mostrar'})
     return post
+}
+
+
+async function editPost(request,response){     
+    const existPost =  await Post.count({
+        where: {
+            id: request.body.id
+        }
+    })
+    if (existPost < 1) return response.status(400).send({status:'this post not'})
+    const newPost = Post.create(
+        request.body
+    )
+    return newPost
 }
 
 module.exports = {
